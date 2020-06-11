a2287dc3ac0dcecb735957af6b722198795022ff
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-06-11 19:11:54 -0300

    Ajustes de listagem de Usuario

--- src/main/java/com/lucaswilliam/meuprimeiroapp/domains/dto/UsuarioListDTO.java
+++ src/main/java/com/lucaswilliam/meuprimeiroapp/domains/dto/UsuarioListDTO.java
@@ -3,12 +3,14 @@ package com.lucaswilliam.meuprimeiroapp.domains.dto;
 import java.io.Serializable;
 
 import com.lucaswilliam.meuprimeiroapp.domains.Usuario;
+import com.lucaswilliam.meuprimeiroapp.domains.enums.TipoCargo;
 
 public class UsuarioListDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	//Attributes
+	private Integer id;
 	private String nome;
 	private String email;
 	private Integer cargo;
@@ -21,6 +23,7 @@ public class UsuarioListDTO implements Serializable{
 	}
 	
 	public UsuarioListDTO(Usuario obj) {
+		this.id = obj.getId();
 		this.nome = obj.getNome();
 		this.email = obj.getEmail();
 		this.cargo = (obj.getCargo() == null) ? null : obj.getCargo().getCode();
@@ -28,7 +31,7 @@ public class UsuarioListDTO implements Serializable{
 		this.primeiroAcesso = obj.getPrimeiroAcesso();
 	}
 
-	public UsuarioListDTO(String nome, String email, Integer cargo, String fotoPerfil, boolean primeiroAcesso) {
+	public UsuarioListDTO(Integer id, String nome, String email, Integer cargo, String fotoPerfil, boolean primeiroAcesso) {
 		this.nome = nome;
 		this.email = email;
 		this.cargo = cargo;
@@ -37,6 +40,14 @@ public class UsuarioListDTO implements Serializable{
 	}
 
 	//Getters and Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
@@ -53,12 +64,12 @@ public class UsuarioListDTO implements Serializable{
 		this.email = email;
 	}
 
-	public Integer getCargo() {
-		return cargo;
+	public TipoCargo getCargo() {
+		return TipoCargo.toEnum(cargo);
 	}
 
-	public void setCargo(Integer cargo) {
-		this.cargo = cargo;
+	public void setCargo(TipoCargo cargo) {
+		this.cargo = cargo.getCode();
 	}
 
 	public String getFotoPerfil() {
